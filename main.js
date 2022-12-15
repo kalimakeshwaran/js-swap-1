ad90ef0e5ca1d3ebef7e38572ba9629d7ff7b41e
kalimakeshwaran <makeshvijay735@gmail.com>
2022-12-15 14:40:17 +0530

    add

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+
+//function swap//
+
+var a,b,c;
+ a=parseInt(prompt("enter the value"))
+ b=parseInt(prompt("enter the values"))
+ c=a;
+ a=b;
+ b=c;
+ function swap(a,b){
+    document.write(a+"<br>")
+    document.write(b+"<br>")
+
+ }
+ swap(a,b)
